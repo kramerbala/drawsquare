9a935ceeaac4695a19d5e35e9f67d0d018bd284c
Kramer Balázs <93663683+kramerbala@users.noreply.github.com>
2023-11-29 13:39:14 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,33 @@
+class Program
+{
+    public static void Main()
+    {
+
+        int count = int.Parse(Console.ReadLine()); //szam beolvasasa es int-e alakitas
+        char op = char.Parse(Console.ReadLine()); //karakter beolvasasa es char-a alakitas
+        
+        DrawSquare(count, op); //fuggvenhivas a szam es a karakter parameterrel
+
+        Console.ReadLine();
+    }
+
+    public static void DrawSquare(int sideLength, char character) //fuggveny deklaracio int char tipusu parameterekkel
+    {
+        for (int i = 0; i < sideLength; i++) // for ciklus a sorokhoz -> i.sor
+        {
+            for (int j = 0; j < sideLength; j++) // for ciklus az oszlopokhoz -> j.oszlop
+            {
+                if (i == 0 || i == sideLength - 1 || j == 0 || j == sideLength - 1) // ha i == 0 (legfelso sor) vagy i == sideLength - 1 (legalso sor) vagy j == 0 (bal oldal) vagy j == sideLength - 1 (jobb oldal)
+                {
+                    Console.Write(character); // karakter kiirasa
+                }
+                else
+                {
+                    Console.Write(" "); // szokoz kiirasa
+                }
+            }
+            Console.WriteLine(); // uj sor, hiszen az adott sor osszes oszlopat kiirtuk (j nem < sideLength (for loop a 16. sorban))
+        }
+    }
+}
 
